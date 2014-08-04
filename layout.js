58b052250efe31198aff47e40fb415ad6c9dde0c
yoyoyohamapi <472285740@qq.com>
2014-08-04 08:15:04 +0800

    顶部导航交互更改

--- layout.js
+++ layout.js
@@ -1,6 +1,13 @@
 $(function(){
 	// 头像拉出菜单
 	$('.headPic').click(function(){
+		if($('#search_content').width()!=0)
+			$("#search_content").animate({
+				width:0,
+				opacity:0
+			},'slow',function(){
+				$("#search_content").hide();
+			});
 		if($('#settingsMenu').width()==0){
 			$('.search').fadeOut();
 			$('#settingsMenu').fadeTo('fast',0,function(){
@@ -22,23 +29,6 @@ $(function(){
 
 	//拉出搜索框
 	$('.search').click(function(){
-		// if($('.headPic').is(':visible')==true){
-		// 	$('.search').fadeToggle('fast',function(){
-		// 		$('.headPic').fadeOut('fast',function(){
-		// 			$('.search').fadeToggle('fast',function(){
-		// 				$('#search_content').fadeToggle();
-		// 			});
-		// 		});
-		// 	});
-		// }else{
-		// 	$('#search_content').fadeToggle('fast',function(){
-		// 		$('.search').fadeToggle('fast',function(){
-		// 			$('.headPic').fadeIn('fast',function(){
-		// 				$('.search').fadeToggle();
-		// 			});
-		// 		});
-		// 	});
-		// }
 		if($("#search_content").width()==0){
 			$("#search_content").fadeTo('fast',0,function(){
 				$("#search_content").animate({
