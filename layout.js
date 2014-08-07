4e2484e52aee4548ea68ac455578581859378a0a
yoyoyohamapi <472285740@qq.com>
2014-08-07 12:43:45 +0800

    滚动条优化，各种CSS优化

--- layout.js
+++ layout.js
@@ -1,4 +1,27 @@
 $(function(){
+	//浏览器内核判断
+	var userAgent = navigator.userAgent.toLowerCase(); 
+
+	// Figure out what browser is being used 
+
+	jQuery.browser = { 
+
+	version: (userAgent.match( /.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/ ) || [])[1], 
+
+	safari: /webkit/.test( userAgent ), 
+
+	opera: /opera/.test( userAgent ), 
+
+	msie: /msie/.test( userAgent ) && !/opera/.test( userAgent ), 
+
+	mozilla: /mozilla/.test( userAgent ) && !/(compatible|webkit)/.test( userAgent ) 
+
+	}; 
+
+	if($(".conPanelCon").length>0 && ($.browser.mozilla || $.browser.msie)){
+		$(".conPanelCon").mCustomScrollbar();
+	}
+
 	// 头像拉出菜单
 	$('.headPic').click(function(){
 		if($('#search_content').width()!=0)
